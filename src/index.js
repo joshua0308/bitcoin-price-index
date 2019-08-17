d293a090f5d5f6a1e7ac5252a2c07b72dea92a1e
Josh Kim <joshua940308@gmail.com>
2019-08-17 10:31:53 -0400

    added dockerfile

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reactime from 'reactime';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+reactime(document.getElementById('root'));
 registerServiceWorker();
